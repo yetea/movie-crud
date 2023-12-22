f1193a21ea6a495d15cad79ff58109d263bf7f39
yetea <yetea.dev@gmail.com>
2023-12-22 10:49:01 +0200

    chore(go.mod): add go.mod file to manage Go dependencies chore(go.sum): add go.sum file to track Go dependencies checksums feat(main.go): create main.go file with basic CRUD operations for movies

---
+++ src/backend/go.mod
@@ -0,0 +1,5 @@
+module github.com/yetea/movie-crud
+
+go 1.21.5
+
+require github.com/gorilla/mux v1.8.1 // indirect
---
+++ src/backend/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main() {
+	r := mux.NewRouter()
+
+	movies = append(
+		movies,
+		Movie{
+			ID:       "1",
+			Isbn:     "43812",
+			Title:    "Movie One",
+			Director: &Director{FirstName: "John", LastName: "Doe"},
+		},
+	)
+
+	movies = append(
+		movies,
+		Movie{
+			ID:       "2",
+			Isbn:     "13485",
+			Title:    "Movie Two",
+			Director: &Director{FirstName: "Jane", LastName: "Doe"},
+		},
+	)
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting server at port 8000\n")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
